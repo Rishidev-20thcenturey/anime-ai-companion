0a1a7779543c74b2eb44912480034d8ef5a7b7f9
Rishi Raj DEV <rishirajc406@gmail.com>
2026-09-07 20:26:30 +0530

    Make RAY config validate latent geometry

--- ray_image/config.py
+++ ray_image/config.py
@@ -16,4 +16,15 @@ class RAYConfig:
 
     @property
     def latent_size(self) -> int:
+        """Spatial latent size for the VAE's fixed 8x compression."""
+        if self.image_size % 8 != 0:
+            raise ValueError("image_size must be divisible by 8")
         return self.image_size // 8
+
+    @property
+    def latent_tokens(self) -> int:
+        """Number of DiT patch tokens produced by the latent grid."""
+        if self.latent_size % self.patch_size != 0:
+            raise ValueError("latent_size must be divisible by patch_size")
+        side = self.latent_size // self.patch_size
+        return side * side
